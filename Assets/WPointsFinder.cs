df27c8ed96d91edc634c3f2fdc07b653cc62572d
speckhere <savannah@tanners.org>
2022-05-07 17:17:22 -0500

    Slug is perfection

--- Assets/WPointsFinder.cs
+++ Assets/WPointsFinder.cs
@@ -6,22 +6,42 @@ public class WPointsFinder : MonoBehaviour
 {
      public float speed;
      private Waypoints Wpoints;
+     public Transform[] waypoints;
 
      private int waypointIndex;
+     private float dist;
 
     void Start()
     {
-        Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
+       waypointIndex = 0;
+       Wpoints = GameObject.FindGameObjectWithTag("Waypoints").GetComponent<Waypoints>();
     }
 
    void Update() 
     {
-        transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
-  
-        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f) 
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
         {
-            waypointIndex++;
+            IncreaseIndex();
         }
+        Patrol();
     }
 
+
+    void Patrol()
+    {
+       transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+        //Vector2.MoveTowards(transform.position, waypoints[waypointIndex].position, speed * Time.deltaTime);
+        //transform.Translate(Vector2.x * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        
+    }
 }
