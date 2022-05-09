97dbb29ac12fd74c67dbfca752ad076653722a34
speckhere <savannah@tanners.org>
2022-05-09 11:28:14 -0500

    We have a moving snail

---
+++ Assets/Sluggy.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sluggy : MonoBehaviour
+{
+     public float speed;
+     private Waypoints Wpoints;
+     public Transform[] waypoints;
+
+     private int waypointIndex;
+     private float dist;
+
+    void Start()
+    {
+       waypointIndex = 0;
+       Wpoints = GameObject.FindGameObjectWithTag("Snail-WPS").GetComponent<Waypoints>();
+    }
+
+   void Update() 
+    {
+
+        if(Vector2.Distance(transform.position, Wpoints.waypoints[waypointIndex].position) < 0.1f)
+        {
+            IncreaseIndex();
+        }
+        Patrol();
+    }
+
+
+    void Patrol()
+    {
+       transform.position = Vector2.MoveTowards(transform.position, Wpoints.waypoints[waypointIndex].position, speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if(waypointIndex >= waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+        
+    }
+}
