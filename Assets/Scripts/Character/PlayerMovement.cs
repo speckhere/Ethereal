9220603d4b26faeb8b9aeed43a2cb3599300687c
Ereneri <ederisgen@gmail.com>
2022-09-16 18:12:07 -0500

    Fixed Mass and Animation bug

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour {
 	public CharacterController2D controller;
 	public Animator animator;
 
-	public float runSpeed = 80f;
+	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
 	bool jump = false;
