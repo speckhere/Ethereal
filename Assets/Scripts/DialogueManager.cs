8c52d6813d012656beb80e408290ba321e0d9850
speckhere <savannah@tanners.org>
2022-09-25 12:12:34 -0500

    Dialogue System READY

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -8,6 +8,8 @@ public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
     
     private Queue<string> sentences;
 
@@ -19,6 +21,8 @@ public class DialogueManager : MonoBehaviour
 
     public void StartDialogue (Dialogue dialogue)
     {
+        animator.SetBool("IsOpen", true);
+        
         nameText.text = dialogue.name;
 
         sentences.Clear();
@@ -40,12 +44,23 @@ public class DialogueManager : MonoBehaviour
         }
 
         string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
     }
+
+    IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
 
     void EndDialogue()
     {
-        Debug.Log ("End of conversation.");
+        animator.SetBool("IsOpen", false);
     }
 
 }
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
 	public Dialogue dialogue;
+    public Animator animator;
+
+    private void Start() 
+    {
+        animator.SetBool ("Clicked", false);
+    }
 
 	public void TriggerDialogue ()
 	{
 		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        animator.SetBool ("Clicked", true);
 	}
 
 }
